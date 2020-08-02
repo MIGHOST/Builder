0f0e32571c5156314975fa22083c3be6ea09d03d
Max <54718475+Fuhrer13@users.noreply.github.com>
2020-08-02 14:35:18 +0300

    one

--- src/Components/PdfDocument/Document2.js
+++ src/Components/PdfDocument/Document2.js
@@ -15,93 +15,57 @@ const Document2 = ({ resume }) => {
     <Document>
       <Page style={styles.body}>
         <View style={styles.wrapper}>
-          <View style={styles.titleSection}>
+          <View style={styles.titleBox}>
             <Text style={styles.title}>
               {basicInfo.firstName} {basicInfo.lastName}
             </Text>
             <Text style={styles.subtitle}>{basicInfo.jobTitle}</Text>
-          </View>
-          <View style={styles.section}>
-            <View style={styles.sectionInfo}>
-              <Text style={styles.infoTitle}>Info</Text>
-              <Text style={styles.infoBorder}></Text>
-              <Text style={styles.infoSubtitle}>Address</Text>
-              <Text style={styles.infoText}>
+            <View style={styles.infoUser}>
+              <Text style={styles.textInfo}>
                 {basicInfo.city}, {basicInfo.country}
               </Text>
-              <Text style={styles.infoSubtitle}>Phone</Text>
-              <Text style={styles.infoText}>{basicInfo.phone}</Text>
-              <Text style={styles.infoSubtitle}>E-mail</Text>
+              <Text style={styles.textInfo}>{basicInfo.phone}</Text>
               <Text style={styles.emailInfo}>{basicInfo.email}</Text>
-
-              <Text style={styles.infoTitle}>Skills</Text>
-              <Text style={styles.infoBorder}></Text>
-              <View>
-                {skills.map((el) => (
-                  <Text style={styles.infoSubtitle} key={el.skill}>
-                    {el.skill}
-                  </Text>
-                ))}
-              </View>
             </View>
-
-            <View style={styles.sectionAbout}>
-              <View style={styles.sectionBorder}>
-                <Text style={styles.aboutTitle}>Profile</Text>
-                <Text style={styles.aboutBorder}></Text>
-                <Text style={styles.aboutText}>{basicInfo.summary}</Text>
-              </View>
-
-              <Text style={styles.aboutTitle}>employment history</Text>
-              <Text style={styles.aboutBorder}></Text>
-              <Text style={styles.aboutSubtitle}>
+          </View>
+          <View style={styles.section}>
+            <View style={styles.profileSection}>
+              <Text style={styles.sectionTitle}>Profile</Text>
+              <Text style={styles.profileText}>{basicInfo.summary}</Text>
+              <Text style={styles.sectionTitle}>employment history</Text>
+              <Text style={styles.profileSubtitle}>
                 {employmentHistory.jobTitle} {employmentHistory.city}
               </Text>
-              <Text style={styles.aboutData}>
+              <Text style={styles.profileData}>
                 {employmentHistory.start}-{employmentHistory.end}
               </Text>
-              <Text style={styles.aboutText}>
+              <Text style={styles.profileText}>
                 {employmentHistory.description}
               </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
+              <Text style={styles.sectionTitle}>Education</Text>
+              <Text style={styles.profileSubtitle}>
+                {educations.school} {educations.degree}
               </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
+              <Text style={styles.profileData}>
+                {educations.start}-{educations.end}
               </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
+              <Text style={styles.profileSubtitle}>
+                {educations.school} {educations.degree}
               </Text>
-              <Text style={styles.aboutSubtitle}>
-                {employmentHistory.jobTitle} {employmentHistory.city}
+              <Text style={styles.profileData}>
+                {educations.start}-{educations.end}
               </Text>
-              <Text style={styles.aboutData}>
-                {employmentHistory.start}-{employmentHistory.end}
-              </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
-              </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
-              </Text>
-              <Text style={styles.aboutText}>
-                {employmentHistory.description}
-              </Text>
+            </View>
+          </View>
+          <View style={styles.skillsSection}>
+            <Text style={styles.sectionTitle}>Skills</Text>
 
-              <Text style={styles.aboutTitle}>Education</Text>
-              <Text style={styles.aboutBorder}></Text>
-              <Text style={styles.aboutSubtitle}>
-                {educations.school} {educations.degree}
-              </Text>
-              <Text style={styles.aboutData}>
-                {educations.start}-{educations.end}
-              </Text>
-              <Text style={styles.aboutSubtitle}>
-                {educations.school} {educations.degree}
-              </Text>
-              <Text style={styles.aboutData}>
-                {educations.start}-{educations.end}
-              </Text>
+            <View>
+              {skills.map((el) => (
+                <Text style={styles.skillsInfo} key={el.skill}>
+                  {el.skill}
+                </Text>
+              ))}
             </View>
           </View>
         </View>
@@ -133,142 +97,149 @@ const styles = StyleSheet.create({
     paddingHorizontal: 35,
   },
 
-  // wrapper: {
-  //   boxShadow:
-  //     "1px 2px 4px rgba(0, 0, 0, 0.06), 1px 3px 4px rgba(0, 0, 0, 0.08), 1px 4px 6px rgba(0, 0, 0, 0.12)",
-  //   width: "100%",
-  // },
+  wrapper: {
+    boxShadow:
+      "1px 2px 4px rgba(0, 0, 0, 0.06), 1px 3px 4px rgba(0, 0, 0, 0.08), 1px 4px 6px rgba(0, 0, 0, 0.12)",
+    width: "100%",
+  },
 
-  titleSection: {
-    borderColor: "rgba(24, 28, 39, 0.1)",
-    borderBottomWidth: 1,
-    borderBottomStyle: "solid",
+  titleBox: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
   },
 
   title: {
-    fontStyle: "normal",
-    fontSize: 48,
-    textTransform: "uppercase",
-    color: " #181C27",
-    letterSpacing: "0.04em",
-    textAlign: "left",
-    marginBottom: 20,
-  },
-
-  subtitle: {
-    fontStyle: "normal",
-    fontWeight: "normal",
-    fontSize: 14,
-    color: "rgba(24, 28, 39, 0.6)",
-    letterSpacing: "0.1em",
-    paddingTop: 16,
-    marginBottom: 40,
-  },
-
-  section: {
+    maxWidth: 319,
+    // fontFamily: "Roboto",
+    // fontStyle: "normal",
+    // fontWeight: 500,
+    fontSize: 36,
+    lineHeight: 42,
     display: "flex",
-    flexWrap: "wrap",
-  },
-
-  infoBorder: {
-    backgroundColor: "black",
-    width: 40,
-    height: 4,
-    marginTop: 3,
-    marginBottom: 40,
-  },
-
-  aboutBorder: {
-    backgroundColor: "black",
-    width: 40,
-    height: 4,
-    marginTop: 3,
-    marginBottom: 40,
-  },
-
-  sectionInfo: {
-    borderColor: "#d3d3d3",
-    borderRightWidth: 1,
-    borderRightStyle: "solid",
-    paddingTop: 40,
-    paddingRight: 50,
-    width: 150,
-    // border: "1px solid rgba(24, 28, 39, 0.1)",
-    // transform: "rotate(-90deg)",
-  },
-
-  infoTitle: {
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 20,
-    letterSpacing: "0.04em",
-    textTransform: "uppercase",
-    color: "#181C27",
-  },
-
-  infoSubtitle: {
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 14,
-    textTransform: "uppercase",
-    letterSpacing: "0.02em",
-    color: "#181C27",
-  },
-
-  infoText: {
-    fontSize: 14,
-    letterSpacing: "0.1em",
-    fontStyle: "normal",
-    color: "rgba(24, 28, 39, 0.6)",
-  },
-
-  emailInfo: {
-    fontSize: 14,
-    color: "rgba(24, 28, 39, 0.6);",
-    marginBottom: 40,
-    textDecoration: "underline",
-  },
-
-  sectionAbout: {
-    paddingTop: 40,
-    paddingLeft: 30,
-  },
-
-  sectionBorder: {
-    borderColor: "rgba(24, 28, 39, 0.1)",
-    borderBottomWidth: 1,
-    borderBottomStyle: "solid",
-  },
-
-  aboutTitle: {
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 20,
-    letterSpacing: "0.04em",
-    textTransform: "uppercase",
-    color: "#181C27",
-  },
-
-  aboutSubtitle: {
-    fontStyle: "normal",
-    fontWeight: 500,
-    fontSize: 14,
-    textTransform: "uppercase",
-    letterSpacing: "0.02em",
-    color: "#181C27",
-  },
-
-  aboutText: {},
-
-  pageNumber: {
-    position: "absolute",
-    fontSize: 12,
-    bottom: 30,
-    left: 0,
-    right: 0,
+    alignItems: "center",
     textAlign: "center",
-    color: "grey",
+    letterSpacing: "0.04em",
+    textTransform: "uppercase",
+
+    color: "#181c27",
   },
+
+  // subtitle: {
+  //   paddingTop: "16",
+
+  //   // fontFamily: "Roboto",
+  //   fontStyle: "normal",
+  //   fontWeight: "normal",
+  //   fontSize: 14,
+  //   lineHeight: 16,
+  //   display: "flex",
+  //   alignItems: "center",
+  //   textAlign: "center",
+  //   letterSpacing: "0.1em",
+  //   textTransform: "uppercase",
+
+  //   color: "rgba(24, 28, 39, 0.6)",
+  // },
+
+  // infoUser: {
+  //   width: "100%",
+  //   paddingTop: "24",
+  //   display: "flex",
+  //   justifyContent: "space-around",
+  // },
+
+  // titleText: {
+  //   // fontFamily: "Roboto",
+  //   fontStyle: "normal",
+  //   fontWeight: "normal",
+  //   fontSize: 14,
+  //   lineHeight: 16,
+  //   display: "flex",
+  //   alignItems: "center",
+  //   textAlign: "center",
+  //   letterSpacing: "0.1em",
+
+  //   color: "rgba(24, 28, 39, 0.6)",
+  // },
+
+  // textInfo: {
+  //   fontSize: 14,
+  //   letterSpacing: "0.1em",
+  //   fontStyle: "normal",
+  //   color: "rgba(24, 28, 39, 0.6)",
+  // },
+
+  // emailInfo: {
+  //   fontSize: 14,
+  //   color: "rgba(24, 28, 39, 0.6);",
+  //   marginBottom: 40,
+  //   textDecoration: "underline",
+  // },
+
+  // section: {
+  //   display: "flex",
+  //   flexDirection: "row",
+  // },
+
+  // profileSection: { display: "flex", flexDirection: "row" },
+
+  // sectionTitle: {
+  //   marginBottom: 32,
+  //   padding: "4, 16",
+  //   // fontFamily: "Roboto",
+  //   fontStyle: "normal",
+  //   fontWeight: "500",
+  //   fontSize: 18,
+  //   lineHeight: 21,
+  //   display: "inline-block",
+  //   alignItems: "center",
+  //   letterSpacing: "0.04em",
+  //   textTransform: "uppercase",
+  //   color: "#ffffff",
+  //   backgroundColor: "rgb(78, 79, 80)",
+  // },
+
+  // sectionSubtitle: {
+  //   paddingTop: 4,
+  //   paddingBottom: 8,
+  //   // fontFamily: "Roboto",
+  //   fontStyle: "normal",
+  //   fontWeight: "normal",
+  //   fontSize: 14,
+  //   lineHeight: 24,
+  //   letterSpacing: "0.02em",
+  //   color: "#181c27",
+  // },
+
+  // profileText: {
+  //   fontSize: 14,
+  //   fontFamily: "Oswald",
+  //   color: "grey",
+  //   paddingBottom: 40,
+  //   marginBottom: 40,
+  //   borderColor: "#d3d3d3",
+  //   borderBottomWidth: 1,
+  //   borderBottomStyle: "solid",
+  // },
+
+  // profileData: {
+  //   fontSize: 14,
+  //   color: "black",
+  //   marginBottom: 16,
+  // },
+
+  // skillsSection: { paddingTop: 40, paddingLeft: 30 },
+
+  // pageNumber: {
+  //   position: "absolute",
+  //   fontSize: 12,
+  //   bottom: 30,
+  //   left: 0,
+  //   right: 0,
+  //   textAlign: "center",
+  //   color: "grey",
+  // },
 });
 
 export default Document2;
