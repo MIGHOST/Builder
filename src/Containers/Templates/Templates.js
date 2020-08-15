91d0ffbf7964f6189c6baffa1602f9de749a08c9
KirillKulakow <kirillkulakow31@gmail.com>
2020-08-15 14:01:30 +0300

    fix templates redirect

--- src/Containers/Templates/Templates.js
+++ src/Containers/Templates/Templates.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./Templates.module.css";
-import { Switch, Route, NavLink, useRouteMatch } from "react-router-dom";
+import { Switch, Route, NavLink, useRouteMatch, Redirect } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { changeType } from "../../redux/actions/basicInfo";
 import Template_1 from "../../Components/ResumeTemplate1/ResumeTemplate1";
@@ -9,14 +9,17 @@ import { paths } from "../../constants";
 // import { set } from "date-fns";
 // import { darkTheme, lightTheme } from "../../redux/actions/theme";
 
-const Templates = ({location}) => {
+const Templates = (props) => {
   const theme = useSelector(state => state.theme);
-  const isResumeOne = location.pathname === paths.templates;
+  const typeOfResume = useSelector(state => state.resume.basicInfo.type);
+  const isResumeOneStyle = props.location.pathname === paths.templates;
+  const isResumeOneRedirect = typeOfResume === 1;
   const path = useRouteMatch().path;
   const dispatch = useDispatch();
   const changeNumber = number => {
     dispatch(changeType(number));
   };
+  console.log(props)
 
   return (
     <div
@@ -27,7 +30,7 @@ const Templates = ({location}) => {
       <div className={styles.LinkWrapper}>
         <NavLink
           to={paths.editor}
-          className={isResumeOne ? styles.LinkNewResumeActive : styles.LinkNewResumeNotActive}
+          className={isResumeOneStyle ? styles.LinkNewResumeActive : styles.LinkNewResumeNotActive}
           onClick={() => changeNumber(1)}
         />
         <NavLink
@@ -49,7 +52,7 @@ const Templates = ({location}) => {
         </NavLink>
         <NavLink
           to={paths.editor}
-          className={isResumeOne ? styles.LinkNewResumeNotActive : styles.LinkNewResumeActive}
+          className={isResumeOneStyle ? styles.LinkNewResumeNotActive : styles.LinkNewResumeActive}
           onClick={() => changeNumber(2)}
         />
         <NavLink
@@ -71,8 +74,10 @@ const Templates = ({location}) => {
       </div>
 
       <Switch>
-        <Route exact path={`${path}`} component={Template_1} />
-        <Route path={`${path}/2`} component={Template_2} />
+        <Route exact path={`${path}`} component={Template_1}>
+          {!isResumeOneRedirect ? <Redirect to={`${path}/2`} component={Template_2}/> : ""}
+        </Route>
+        <Route path={`${path}/2`} component={Template_2}/>
       </Switch>
     </div>
   );
